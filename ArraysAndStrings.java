9f19a8d9045049e1880d20fde5f7110a3a25e554
Nicolas Rockwood <nar2163@columbia.edu>
2020-05-24 17:07:26 -0400

    Practice for May 24

--- ArraysAndStrings.java
+++ ArraysAndStrings.java
@@ -9,10 +9,71 @@ public class ArraysAndStrings{
 //        System.out.println(uniqueChar("car"));
 //        System.out.println(uniqueChar("ham"));
 
-        System.out.println(isPerm("car", "arc"));
-        System.out.println(isPerm("far", "wide"));
-        System.out.println(isPerm("bus", "bus"));
+//        System.out.println(isPerm("car", "arc"));
+//        System.out.println(isPerm("far", "wide"));
+//        System.out.println(isPerm("bus", "bus"));
 
+//        System.out.println(urlify("Mr John Smith", 13));
+//        System.out.println(urlify("nicrockwood .com", 15));
+//        System.out.println(urlify("www.google.com", 14));
+
+        System.out.println(palindromePerm("Tact Coa"));
+        System.out.println(palindromePerm("abcdsef"));
+        System.out.println(palindromePerm("ababa"));
+        System.out.println(palindromePerm("ababac"));
+        System.out.println(palindromePerm("acb bca"));
+
+
+    }
+
+    public static boolean palindromePerm(String word){
+        word = word.toLowerCase();
+        Hashtable<Character, Integer> letters = new Hashtable<Character, Integer>();
+        boolean perm = false;
+        int tracker = 0;
+
+        for(int i = 0; i < word.length(); i++){
+            if(word.charAt(i) == ' '){
+                continue;
+            }
+            if(!letters.containsKey(word.charAt(i))){
+                letters.put(word.charAt(i), 1);
+            } else {
+                letters.replace(word.charAt(i), letters.get(word.charAt(i)) + 1);
+            }
+        }
+
+        Set<Character> keys = letters.keySet();
+
+        for(Character c : keys){
+            if(letters.get(c) % 2 == 0) {
+                perm = true;
+            } else if(letters.get(c) % 2 != 0){
+                tracker++;
+            }
+        }
+
+        if(tracker > 1){
+            perm = false;
+        }
+
+        return perm;
+
+    }
+
+    public static String urlify(String url, int length){
+
+        StringBuilder urlify = new StringBuilder();
+
+        for(int i  = 0; i < url.length(); i++){
+            if(url.charAt(i) == ' '){
+                urlify.append("%20");
+            } else {
+                urlify.append(url.charAt(i));
+            }
+        }
+
+        return urlify.toString();
     }
 
     public static boolean uniqueChar(String word){
