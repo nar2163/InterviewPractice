7fb97834ca9bd5f7d004e68f5a5f4bcbd55ace05
Nicolas Rockwood <nar2163@columbia.edu>
2020-08-19 17:05:07 -0700

    have Quick and Selection sorts

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,11 +1,32 @@
+import java.util.*;
+
 public class SelectionSort{
 
     public static void main(String[] args){
 
+        int[] testArray = {3, 5, 8, 1, 0, 2, 4};
+
+        selectionSort(testArray);
+
+        System.out.println(Arrays.toString(testArray));
+
     }
 
-    public selectionSort(int[] arr){
+    public static void selectionSort(int[] arr){
 
+        for(int i = 0; i < arr.length - 1; i++){
+            int currentMinIndex = i;
+
+            for(int j = currentMinIndex; j < arr.length; j++){
+                if(arr[j] < arr[currentMinIndex]){
+                    currentMinIndex = j;
+                }
+            }
+
+            int temp = arr[currentMinIndex];
+            arr[currentMinIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 
 }
